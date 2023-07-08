e006f69696e634260a448f4f170758ab03489dc1
Dmitriifd <idimka9@gmail.com>
2023-07-08 16:13:50 +0500

    portfolio page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import styles from './page.module.css';
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
+import { Item, Items, items } from './data.ts';
+import { notFound } from 'next/navigation';
 
-const Category = () => {
-  return <div>Category</div>;
+const getData = (cat: keyof Items): Item[] | undefined => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
+interface CategoryProps {
+  params: {
+    category: string;
+  };
+}
+
+const Category = ({ params }: CategoryProps) => {
+  const data = getData(params.category as keyof Items);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {data?.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text='See More' url='#' />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt='' />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
