c851d4997c1ddcdedc907d5378121c9e38f316ed
Dmitriifd <idimka9@gmail.com>
2023-07-08 23:53:56 +0500

    next auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'test.com'],
+    domains: ['images.pexels.com', 'test.com', 'images.unsplash.com'],
   },
 };
 
